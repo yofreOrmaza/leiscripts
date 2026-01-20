4a32608f8e32fc5b584c9e3e9a79cf1cdc100b40
Yofre <97921335+yofreOrmaza@users.noreply.github.com>
2026-01-20 18:55:36 +0000

    fix: Remove repo owner values of default dotrec config from dotrecdefault.sh
    Clear default microphone, audio, resolution, frame rate, and coordinates settings.

--- scripts/dotrec/dotrecscripts/dotrecdefault.sh
+++ scripts/dotrec/dotrecscripts/dotrecdefault.sh
@@ -9,13 +9,13 @@ xrandr | grep " connected"
 echo
 
 # pactl list short sources
-mic=alsa_input.pci-0000_03_00.6.analog-stereo
-pcAudio=alsa_output.pci-0000_03_00.6.analog-stereo.monitor
+mic=
+pcAudio=
 videoFormat=mkv
 
-resolucionDeMonitor=1366x768 # -video_size
-framerateDeGrabacion=25 # -framerate
-coordenadasDeGrabacion=1366,0 # Coordenadas de Monitor (-i :0.0+X,Y) -i :0.0+
+resolucionDeMonitor=
+framerateDeGrabacion=
+coordenadasDeGrabacion=
 read -p "Nombre de Archivo de Grabación: " nombreDeArchivoDeGrabacion
 nombreDeArchivoFinal=${nombreDeArchivoDeGrabacion:-salida} # Esta variable de "Final" se crea con el objetivo de que si el valor del nombre de grabación solicitado al usuario es vacio, pues para no causar errores se le asigna un valor por defecto
 
